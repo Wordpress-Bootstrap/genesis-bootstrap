c98a447a5c8dae416e437f50ff5d972c10a79c6c
Bryan Willis <willis.bryan@live.com>
2016-03-07 22:21:18 -0600

    Update genesis-setup.php

--- lib/functions/genesis-setup.php
+++ lib/functions/genesis-setup.php
@@ -1,29 +1,34 @@
 <?php
 /** 
  * Setup Genesis Defaults for Bootstrap Plugin and Themes
- * 
  */
 
-remove_theme_support( 'genesis-accessibility' );
+// remove_theme_support( 'genesis-accessibility' );
 
-/** Remove Parent Theme Support */
-
-/** Hide Wordpress Specific Schema */
-// remove_filter( 'genesis_attr_site-header', 'genesis_attributes_header' );
-// remove_filter( 'genesis_attr_site-footer', 'genesis_attributes_site_footer' );
-
-/** Remove Superfish */	
+//* Remove Superfish
+function sp_disable_superfish() {
+	wp_deregister_script( 'superfish' );
+	wp_deregister_script( 'superfish-args' );
+}
 add_action( 'wp_enqueue_scripts', 'sp_disable_superfish' );
 	
 /** Add featured image sizes */
 add_image_size( 'bsg-featured-image', 1170, 630, true );
 
-
 /** Custom Post Info and Meta */
-add_filter( 'genesis_post_info', 'sp_post_info_filter' );
 add_filter( 'genesis_post_meta', 'sp_post_meta_filter' );
+function sp_post_info_filter($post_info) {
+	$post_info = '[post_date format="j F, Y" before="<span class=\'post-date text-muted\'>" after="</span>" label="<i class=\'fa fa-clock-o\'></i> "] by [post_author_posts_link] [post_comments before="<span class=\'pull-right\'><span class=\'text-muted fa fa-comments\'></span>" after="</span>" zero=" Leave a Comment" one=" 1 Comment" more=" % Comments"]';
+	return $post_info;
+}
 
+add_filter( 'genesis_post_info', 'sp_post_info_filter' );
+function sp_post_meta_filter($post_info) {
+	$post_info = '';
+	return $post_info;
+}
 
+//* Add theme support
 add_theme_support( 'html5', array( 
 	'comment-list', 
 	'comment-form', 
@@ -31,48 +36,10 @@ add_theme_support( 'html5', array(
 	'gallery', 
 	'caption'  
 ) );
-	
 
-add_theme_support ( 'genesis-menus' , array ( 
-	'primary' => __( 'Primary Navigation Menu', 'genesis' ),
-	'secondary' => __( 'Secondary Navigation Menu', 'genesis' ),
-	// Add New Footer Menu; Keep Primary and Secondary Menus
-	'footer' => __( 'Footer Navigation Menu', 'genesis' )
-	) );
-	
-
-function sp_post_info_filter($post_info) {
-	$post_info = '[post_date format="j F, Y" before="<span class=\'post-date text-muted\'>" after="</span>" label="<i class=\'fa fa-clock-o\'></i> "] by [post_author_posts_link] [post_comments before="<span class=\'pull-right\'><span class=\'text-muted fa fa-comments\'></span>" after="</span>" zero=" Leave a Comment" one=" 1 Comment" more=" % Comments"]';
-	return $post_info;
-}
-
-function sp_post_meta_filter($post_info) {
-	$post_info = '';
-	return $post_info;
-}
-
-function sp_disable_superfish() {
-	wp_deregister_script( 'superfish' );
-	wp_deregister_script( 'superfish-args' );
-}
-
-// Remove dynamic logo/text from admin customizer
-add_action( 'customize_register', 'bsg_remove_genesis_customizer_controls', 20 );
+//* Remove dynamic logo/text from admin customizer
 function bsg_remove_genesis_customizer_controls( $wp_customize ) {
     $wp_customize->remove_control( 'blog_title' );
     return $wp_customize;
 }
-
-// Remove page templates
-add_filter( 'theme_page_templates', 'bsg_be_remove_genesis_page_templates' );
-function bsg_be_remove_genesis_page_templates( $page_templates ) {
-	unset( $page_templates['page_archive.php'] );
-	unset( $page_templates['page_blog.php'] );
-	return $page_templates;
-}
-
-// Remove header settings from genesis admin screens
-add_action( 'genesis_admin_before_metaboxes', 'bsg_remove_genesis_theme_metaboxes' );
-function bsg_remove_genesis_theme_metaboxes( $hook ) {
-    remove_meta_box( 'genesis-theme-settings-header',   $hook, 'main' );
-}
+add_action( 'customize_register', 'bsg_remove_genesis_customizer_controls', 20 );
